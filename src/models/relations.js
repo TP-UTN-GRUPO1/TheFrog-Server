db13e6ccfba284cf02451c97c7857c9c7623ae96
unknown <qalopez2000@gmail.com>
2025-05-03 15:36:10 -0300

    -

--- src/models/relations.js
+++ src/models/relations.js
@@ -1,7 +1,18 @@
 // aca vendriamos a poner todo lo que son las relaciones de las tablas si es 1 n || n n
-import {Role} from "./Roles.js"
-import {User} from "./Users.js"
+import {Role} from "./Roles.js";
+import {User} from "./Users.js";
+import {Game} from "./Games.js";
+import {Genre} from "./Genre.js";
+import {Platform} from "./Platform.js";     
+
 
 
 Role.hasMany(User);      // un rol puede tener muchos usuarios
 User.belongsTo(Role);    // un usuario tiene un solo rol
+
+Game.belongsToMany(Genre, {through: "game_genre"}); // un juego puede tener muchos generos y un genero puede tener muchos juegos
+Genre.belongsToMany(Game, {through: "game_genre"}); // un genero puede tener muchos juegos y un juego puede tener muchos generos
+
+Game.belongsToMany(Platform, {through: "game_platform"}); // un juego puede tener muchas plataformas y una plataforma puede tener muchos juegos
+Platform.belongsToMany(Game, {through: "game_platform"}); // una plataforma puede tener muchos juegos y un juego puede tener muchas plataformas
+
