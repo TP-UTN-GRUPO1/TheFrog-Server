779bd825f521359faf329b983e287fd54206dfed
MartinSaporiti99 <martinsaporiti45@gmail.com>
2025-05-21 12:04:24 -0300

    fix sv register/login & add auth login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,72 +1,57 @@
 import { User } from "../models/Users.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { validateEmail, validatePassword} from "../utils/validations.js";
-
 
 export const createNewUser = async (req, res) => {
-    console.log("Cuerpo recibido:", req.body);console.log("Cuerpo recibido:", req.body);
-    const { name, email, date, password} = req.body;
+  console.log("Cuerpo recibido:", req.body);
+  console.log("Cuerpo recibido:", req.body);
+  const { name, email, date, password } = req.body;
 
-    const existingUser = await User.findOne({ where: { email } });
+  const existingUser = await User.findOne({ where: { email } });
 
-    if (existingUser) {
-        return res.status(400).send({ message: "El email ya está registrado" });
-    }
+  if (existingUser) {
+    return res.status(400).send({ message: "El email ya está registrado" });
+  }
 
-    // Hash password
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  // Hash password
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = await User.create({
-        name,
-        email,
-        date,
-        password: hashedPassword,
-    });
+  const newUser = await User.create({
+    name,
+    email,
+    date,
+    password: hashedPassword,
+  });
 
-    res.status(201).json({ id: newUser.id });
+  res.status(201).json({ id: newUser.id });
 };
 
 export const loginUser = async (req, res) => {
-    if (!validateLoginUser(req.body))
-        return res.status(400).send({ message: "Hubo un error en la solicitud" });
+  const { email, password } = req.body;
 
-    const { email, password} = req.body;
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
 
-    const user = await User.findOne({
-        where: {
-            email
-        }
-    });
+  if (!user) return res.status(401).send({ message: "Usuario no existente" });
 
-    if (!user)
-        return res.status(401).send({message: "Usuario no existente"});
+  const comparison = await bcrypt.compare(password, user.password);
 
-    const comparison = await bcrypt.compare (password, user.password);
+  if (!comparison)
+    return res.status(401).send({ message: "Email y/o contraseña incorrecta" });
 
-    if(!comparison)
-        return res.status(401).send({ message: "Email y/o contraseña incorrecta"});
+  //Generate token
 
-    //Generate token
+  const secretKey = "theFrogGames";
+  const token = jwt.sign(
+    { id: user.id, email: user.email, name: user.name },
+    secretKey,
+    { expiresIn: "1h" }
+  );
 
-    const secretKey = "theFrogGames"; 
-    const token = jwt.sign(
-      { id: user.id, email: user.email, name: user.name },
-      secretKey,
-      { expiresIn: "1h" }
-    );
-
-    return res.json(token)
-}
-
-
-const validateLoginUser = ({ email, password }) => {
-    if (!validateEmail(email))
-        return false;
-    else if (!validatePassword(password, 6, 20, true, true))
-        return false;
-
-    return true;
-}
+  return res.json(token);
+};
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,33 +1,55 @@
 export const validateString = (str, minLength, maxLength) => {
+  if (minLength && str.length < minLength) return false;
+  else if (maxLength && str.length > maxLength) return false;
 
-    if (!str)
-        return false;
-    if (minLength && str.length < minLength)
-        return false;
-    else if (maxLength && str.length > maxLength)
-        return false;
-
-    return true;
-} 
+  return true;
+};
 
 export const validateEmail = (email) => {
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    return emailRegex.test(email);
-}
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
-export const validatePassword = (password, minLength, maxLength, needsUpperCase, needsNumber, needsSpecialChar) => {
-    if (!password)
-        return false;
-    if (minLength && password.length < minLength)
-        return false;
-    else if (maxLength && password.length > maxLength)
-        return false;
-    else if (needsUpperCase && !/[A-Z]/.test(password))
-        return false;
-    else if (needsNumber && !/\d/.test(password))
-        return false;
-    else if (needsSpecialChar && !/[!@#$%^&*(),.?":{}|<>]/.test(password)) 
+export const validatePassword = (
+  password,
+  minLength,
+  maxLength,
+  needsUpperCase,
+  needsNumber,
+  needsSpecialChar
+) => {
+  if (minLength && password.length < minLength) return false;
+  else if (maxLength && password.length > maxLength) return false;
+  else if (needsUpperCase && !/[A-Z]/.test(password)) return false;
+  else if (needsNumber && !/\d/.test(password)) return false;
+  else if (needsSpecialChar && !/[!@#$%^&*(),.?":{}|<>]/.test(password))
     return false;
 
-    return true;
-}
+  return true;
+};
+
+export const loginUser = async (req, res) => {
+  const result = validateLoginUser(req.body);
+
+  if (result.error) return res.status(400).send({ message: result.error });
+};
+
+const validateLoginUser = (req) => {
+  const result = {
+    error: false,
+    message: "",
+  };
+  const { email, password } = req;
+  if (!email || !validateEmail(email))
+    return {
+      error: true,
+      message: "Mail invalido",
+    };
+  else if (!password || !validatePassword(password, 7, null, true, true)) {
+    return {
+      error: true,
+      message: "Contraseña invalida",
+    };
+  }
+  return result;
+};
