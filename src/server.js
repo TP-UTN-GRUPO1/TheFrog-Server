20fe2e272aecd319b65b1c60a8ac626f1a6be149
QuimeyAlejo <qalopez2000@gmail.com>
2025-06-09 15:26:41 -0300

    arregle el problema que habia en server.js se pisaba la info en la bd cada vez que se reiniciaba el sv

--- src/server.js
+++ src/server.js
@@ -12,15 +12,16 @@ import "./models/relations.js";
 import "./models/OrderItem.js";
 import "./models/Order.js";
 
-import { addGameFromArchive } from "./controllers/gamesController.js";
-import ordersRoutes from "./routes/order.routes.js";
 import gamesRoutes from "./routes/games.routes.js";
 import usersRoutes from "./routes/users.routes.js";
-import favoritesRoutes from "./routes/favorites.routes.js";
+import favoritesRoutes from "./routes/favorites.routes.js"
 import { loadGenresAndPlatform } from "./controllers/platformGenresController.js";
 import { User } from "./models/Users.js";
 import { Game } from "./models/Games.js";
 import { uploadRolesInDb } from "./controllers/usersController.js";
+import ordersRoutes from "./routes/order.routes.js";
+
+
 
 const app = express();
 app.use(cors());
@@ -31,31 +32,18 @@ app.use(ordersRoutes);
 app.use(gamesRoutes);
 app.use(usersRoutes);
 app.use(favoritesRoutes);
-/*mas que nada aca cuando nos traemos ejemploRoutes lo que haces es traernos todas las rutas /ejemplo y sus metodos para podes utulizarlos
-mas que nada tmb para mantener un orden , buenas practicas y un clean code , igual meter 100 comentarios no se que tan bueno es*/
 
-// lo meti en un func para manejar errores de manera mas facil y despues los await son para esperar la sincronizacion de la base de datos y la carga de datos iniciales antes de iniciar el servidor
 const startServer = async () => {
   try {
-    // Sincronizar la base de datos
-    //await User.drop();
-    //await Game.drop()  // elimina la tabla si existe
-    //await User.sync({ alter: true });
-    await sequelize.sync({ force: true });
-    //  { force: true } para resetear la BD alter:true es para modificar tablas
+    await sequelize.sync(); // { force: true } para resetear la BD alter:true es para modificar tablas
     console.log("DB connect ✅✅✅");
-    await loadGenresAndPlatform();
-    await addGameFromArchive({
-      status: (code) => ({
-        json: (body) =>
-          console.log(`Carga inicial juegos (${code}):`, body.message || body),
-      }),
-    });
-    await uploadRolesInDb();
-    console.log("Genres and platforms loaded ✅✅✅");
+
+    //await loadGenresAndPlatform();
+    //await uploadRolesInDb();
+    //console.log("Genres and platforms loaded ✅✅✅");
 
     app.listen(PORT, () => {
-      console.log(`✅ Server listening on http://localhost:${PORT}`);
+      console.log(`✅ Server listening on http://localhost:${PORT}/games`);
     });
   } catch (error) {
     console.error("There was an error on initialization ❌", error);
