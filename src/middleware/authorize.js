ccaf573b43e4ddcb4824e8b27145b4e2620c23af
QuimeyAlejo <qalopez2000@gmail.com>
2025-06-12 19:01:36 -0300

    revisar el token y realizar modificaciones correspondientes

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,8 +1,8 @@
 
 export const authorize = (...allowedRoles) => {
     return (req, res, next) => {
-      const userRole = req.user?.Role?.roleName;
-  
+      const userRole = req.user?.role?.roleName;
+      console.log(req)
       if (!userRole || !allowedRoles.includes(userRole)) {
         return res.status(403).json({ message: "No tienes permiso para acceder" });
       }
